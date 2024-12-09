cdddb267265ce1eb4f99a19cd28f362ee43f62eb
eduardamendes <eduardamendes2410@gmail.com>
2024-12-09 19:51:18 -0300

    Teste correto

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -23,7 +23,7 @@ public class CalculatorTest {
  
     @Test
     public void testMultiply() {
-        assertEquals(4, calculator.multiply(2, 3));
+        assertEquals(6, calculator.multiply(2, 3));
     }
 
     @Test
